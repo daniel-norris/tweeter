6eb98004e63629fda582c245e069c387a88fc940
Daniel Norris <dan.norris@hotmail.com>
2020-07-28 20:09:16 +0100

    added friends list and sidebar link views

---
+++ resources/views/_friends-list.blade.php
@@ -0,0 +1,14 @@
+<h3 class="font-bold text-xl mb-4">Friends</h3>
+
+<ul>
+    <li>
+        <div class="flex items-center">
+            <img
+                src="https://i.pravatar.cc/40"
+                alt=""
+                class="rounded-full mr-2"
+            >
+            John Doe
+        </div>
+    </li>
+</ul>
