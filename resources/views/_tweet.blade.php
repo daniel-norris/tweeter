1159c74e163548f913db08b57d3020e34417ba60
Daniel Norris <dan.norris@hotmail.com>
2020-07-28 21:40:58 +0100

    added timeline section

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,19 @@
+<div class="flex p-4 border-b border-b-grey-400">
+
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/50"
+            alt=""
+            class="rounded-full mr-2"
+        >
+    </div>
+
+    <div>
+        <h5 class="font-bold">John Doe</h5>
+
+        <p class="text-sm py-4">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere assumenda neque voluptatibus totam expedita veniam deserunt, nostrum voluptas rem quia at magni, ut obcaecati accusantium enim dolor. Maiores, ullam odit.
+        <p>
+    </div>
+
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,31 +9,16 @@
         </div>
 
         <div class="lg:flex-1 lg:mx-10">
-            <div class="border border-blue-400 rounded-lg px-8 py-6">
-                <form action="">
-                    <textarea
-                        name="body"
-                        class="w-full"
-                        placeholder="Whatcha twooting today?"
-                    ></textarea>
 
-                    <hr class="my-4">
+            @include ('_publish-tweet-panel')
 
-                    <footer class="flex justify-between">
-                        <img
-                            src="https://i.pravatar.cc/40"
-                            alt=""
-                            class="rounded-full mr-2"
-                        >
+            <div class="border border-gray-300 rounded-lg">
 
-                        <button class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white" type="submit">Twoot</button>
+                @include('_tweet')
+                @include('_tweet')
+                @include('_tweet')
+                @include('_tweet')
 
-                    </footer>
-
-
-            </div>
-
-            <div>
 
             </div>
 
