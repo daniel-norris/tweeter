defda49838ab52dbdb24e61a37493cf342ddce73
Daniel Norris <dan.norris@hotmail.com>
2020-07-29 00:22:23 +0100

    updated the tweet controller and model

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -3,7 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Tweet;
 
 class TweetController extends Controller
 {
+    public function store()
+    {
+
+        request()->validate(['body' => 'required|max:255']);
+
+        Tweet::create([
+            'user_id' => auth()->id(),
+            'body' => request('body'),
+        ]);
+
+        return redirect('/home');
+    }
 }
--- app/Tweet.php
+++ app/Tweet.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
+    // opposite of $fillable and acts as a blacklist for attributes that cannot be mass assigned
+    // left it empty temporarily
+    protected $guarded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
