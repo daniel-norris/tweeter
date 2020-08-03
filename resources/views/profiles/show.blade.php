30d1cf61b8faedb9c8df1c9c199ffb144c6378c8
Daniel Norris <dan.norris@hotmail.com>
2020-08-03 12:54:55 +0100

    added a bio to profile

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -9,7 +9,7 @@
             class="mb-2"
         >
 
-        <div class="flex justify-between items-center">
+        <div class="flex justify-between items-center mb-4 ">
             <div>
                 <h2 class="font-bold text-2xl mb-0">{{ $user->name }} </h2>
                 <p class="text-sm ">Joined {{ $user->created_at->diffForHumans() }}</p>
@@ -21,12 +21,17 @@
             </div>
         </div>
 
+        <p class="text-sm">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur excepturi possimus quis ea quia eaque, doloremque consequuntur assumenda fuga perferendis repellat neque laboriosam nobis, officiis natus eligendi impedit in omnis?
+        </p>
+
         <img
             src="{{ $user->avatar }}"
             alt=""
             class="rounded-full mr-2 absolute"
-            style="width: 150px; left: calc(50% - 75px); top: 47%;"
+            style="width: 150px; left: calc(50% - 75px); top: 138px;"
         >
+
     </header>
 
     @include ('_timeline', [
