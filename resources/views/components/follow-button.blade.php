8b0b4c4cf0eccb2160828b2da36eefdd344a5bfa
Daniel Norris <dan.norris@hotmail.com>
2020-08-04 12:56:14 +0100

    added conditional for follow button on current user

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,9 +1,13 @@
-<form method="POST" action="/profiles/{{ $user->name }}/follow">
-    @csrf
-    <button
-        type="submit"
-        class="rounded-full border border-gray-300 py-2 px-4 text-black text-xs"
-    >
-        {{ auth()->user()->following($user) ? "Unfollow Me" : "Follow Me" }}
-    </button>
-</form>
+@unless (auth()->user()->is($user))
+
+    <form method="POST" action="/profiles/{{ $user->name }}/follow">
+        @csrf
+        <button
+            type="submit"
+            class="rounded-full border border-gray-300 py-2 px-4 text-black text-xs ml-2"
+        >
+            {{ auth()->user()->following($user) ? "Unfollow Me" : "Follow Me" }}
+        </button>
+    </form>
+
+@endunless
