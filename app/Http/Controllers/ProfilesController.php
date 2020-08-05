9f38479d2dd502be0784c1e59eec62f9d4a8793b
Daniel Norris <dan.norris@hotmail.com>
2020-08-05 15:26:56 +0100

    refactored profiles controller to use a policy for authorisation

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -14,8 +14,8 @@ class ProfilesController extends Controller
 
     public function edit(User $user)
     {
-        abort_if($user->isNot(current_user()), 404);
-    
+        $this->authorize('edit', $user);
+
         return view('profiles.edit', compact('user'));
     }
 }
